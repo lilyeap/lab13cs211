3cb5f54229dfce362e0c5edc3682d28bf88c2cfc
LilyEap1 <>
2022-11-16 18:35:37 +0000

    add newfile and some fixes

---
+++ .ccls-cache/@home@runner@lab12-1/211-f22-collab-github@collab.c
@@ -0,0 +1,31 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void aaqui2_leap2() {
+  printf("Angelo Aquino and Lily Eap\n");
+  printf("Git was very difficult to deal with so we used replit instead.\n");
+}
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    aaqui2_leap2();
+    
+    return 0;
+}
